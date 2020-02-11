0f87baff62482ea79a0935fce56d281227491c05
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-02-11 11:17:32 -0300

    tesseract service return text from captcha

--- src/tesseract/tesseract.service.ts
+++ src/tesseract/tesseract.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as download from 'image-downloader';
-import { from } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
+import { map, mergeMap, take } from 'rxjs/operators';
 import { createWorker, PSM } from 'tesseract.js';
 
 
@@ -21,29 +21,32 @@ export class TesseractService {
             fs.mkdirSync(dir);
         }
 
-        return from(download.image(options)).pipe(map((response: any) => response.filename))
+        return from(download.image(options)).pipe(map((response: any) => response.filename), take(1))
     }
 
-    recognizeCaptchaSchedule(session: string){
-        this.downloadCaptchaImage(session).subscribe(filepath => {
-            const worker = createWorker({
-                langPath: `${__dirname}/../assets`,
-                gzip: false,
-                logger: m => console.log(m)
-            });
+    async recognizeCaptcha(filepath: string): Promise<string>{
+        const worker = createWorker({
+            langPath: `${__dirname}/../assets`,
+            gzip: false,
+            logger: m => console.log(m)
+        });
 
-            (async () => {
-                await worker.load();
-                await worker.loadLanguage('por');
-                await worker.initialize('por');
-                await worker.setParameters({
-                    tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRST',
-                    tessedit_pageseg_mode: PSM.SINGLE_WORD
-                });
-                const result = await worker.recognize(filepath);
-                console.log(result.data.text, result.data.confidence);
-                await worker.terminate();
-            })();
-        })
+        await worker.load();
+        await worker.loadLanguage('por');
+        await worker.initialize('por');
+        await worker.setParameters({
+            tessedit_char_whitelist: 'ABCDEFGHIJKLMNOPQRSTUVWXIZ',
+            tessedit_pageseg_mode: PSM.SINGLE_WORD
+        });
+        const result = await worker.recognize(filepath);
+        await worker.terminate();
+        return result.data.text;
+    }
+
+    getCaptchaSchedule(session: string): Observable<string> {
+        return this.downloadCaptchaImage(session).pipe(
+            mergeMap(filepath => from(this.recognizeCaptcha(filepath))),
+            take(1)
+        )
     }
 }
