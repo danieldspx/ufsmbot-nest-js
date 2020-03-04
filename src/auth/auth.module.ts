653a04324db6da2163d3cab59afae0df34c98ecb
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-03-04 13:39:02 -0300

    Now modules are correct

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from 'src/database/database.module';
+import { RestaurantModule } from 'src/restaurant/restaurant.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, RestaurantModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
