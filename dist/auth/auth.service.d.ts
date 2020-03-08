289749b6c16685c33ca89d6ba4928be0c47035d5
Daniel dos Santos Pereira <danield1591998@gmail.com>
2020-03-08 16:01:27 -0300

    Remove dist (no need)

--- dist/auth/auth.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { DatabaseService } from 'src/database/database.service';
-export declare class AuthService {
-    private readonly dbService;
-    constructor(dbService: DatabaseService);
-    getCustomToken(matricula: any): import("rxjs").Observable<{
-        token: string;
-        message: string;
-    }>;
-}
--- dist/restaurant/request/request.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { Observable } from 'rxjs';
-import { RequestConfig } from './request-config.interface';
-export declare class RequestService {
-    makeRequest(requestConfig: RequestConfig): Observable<Response>;
-}
